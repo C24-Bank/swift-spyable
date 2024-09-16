934bfdee7fe17d52541ea07ebce4e8c41001ac2d
Ibrahim Yilmaz <ibrahim.yilmaz@c24.de>
2024-09-16 13:33:50 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,11 +6,7 @@ import PackageDescription
 let package = Package(
   name: "swift-spyable",
   platforms: [
-    .macOS(.v10_15),
-    .iOS(.v13),
-    .tvOS(.v13),
-    .watchOS(.v6),
-    .macCatalyst(.v13),
+    .iOS(.v13)
   ],
   products: [
     .library(
